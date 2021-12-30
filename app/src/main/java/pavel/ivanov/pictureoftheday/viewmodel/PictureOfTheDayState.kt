9785330d6f65937601c4a2aa69956c8eaeb459e0
KilliLogan <ivanov-p-a@bk.ru>
2021-12-30 10:58:43 +0300

    Layouts:  Create GuideLine, MotionLayout, Collapse, Barrier

--- app/src/main/java/pavel/ivanov/pictureoftheday/viewmodel/PictureOfTheDayState.kt
+++ app/src/main/java/pavel/ivanov/pictureoftheday/viewmodel/PictureOfTheDayState.kt
@@ -3,7 +3,7 @@ package pavel.ivanov.pictureoftheday.viewmodel
 import pavel.ivanov.pictureoftheday.repository.PictureOfTheDayResponseData
 
 sealed class PictureOfTheDayState {
-    data class Success(val pictureOfTheDayResponseData: PictureOfTheDayResponseData):PictureOfTheDayState()
-    data class Loading(val progress: Int?):PictureOfTheDayState()
-    data class Error(val error: Throwable):PictureOfTheDayState()
+    data class Success(val pictureOfTheDayResponseData: PictureOfTheDayResponseData) : PictureOfTheDayState()
+    data class Loading(val progress: Int?) : PictureOfTheDayState()
+    data class Error(val error: Throwable) : PictureOfTheDayState()
 }
